f08a5f3097404c2389c815cb1001dda043523b03
egorsv1985 <egorvs2009@gmail.com>
2023-07-06 14:32:21 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,10 @@ const sourcemaps = require("gulp-sourcemaps");
 const groupMedia = require("gulp-group-css-media-queries");
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
-const path = require("path");
+const webpack = require("webpack-stream");
+const babel = require("gulp-babel");
+const imagemin = require("gulp-imagemin");
+const changed = require("gulp-changed");
 
 const fileIncludeSettings = {
   prefix: "@@",
@@ -27,15 +30,17 @@ const plumberNotify = (title) => {
 
 gulp.task("html", function () {
   return gulp
-    .src("./src/html/pages/*.html")
+    .src("./src/html/**/*.html")
+    .pipe(changed("./dist/"))
     .pipe(fileInclude({ fileIncludeSettings }))
     .pipe(gulp.dest("./dist/"))
-    .pipe(plumber(plumberConfig("HTML")));
+    .pipe(plumber(plumberNotify("HTML")));
 });
 
 gulp.task("sass", function () {
   return gulp
     .src("./src/scss/**/*.scss")
+    .pipe(changed("./dist/css/"))
     .pipe(plumber(plumberNotify("SASS")))
     .pipe(sourcemaps.init())
     .pipe(sass())
@@ -45,17 +50,36 @@ gulp.task("sass", function () {
 });
 
 gulp.task("images", function () {
-  return gulp.src("./src/img/**/*.*").pipe(gulp.dest("./dist/img/"));
+  return gulp
+    .src("./src/img/**/*.*")
+    .pipe(changed("./dist/img/"))
+    .pipe(imagemin({ verbose: true }))
+    .pipe(gulp.dest("./dist/img/"));
 });
 
 gulp.task("fonts", function () {
-  return gulp.src("./src/fonts/**/*.*").pipe(gulp.dest("./dist/fonts/"));
+  return gulp.src("./src/fonts/**/*.*")
+  .pipe(changed("./dist/fonts/"))
+  .pipe(gulp.dest("./dist/fonts/"));
 });
 gulp.task("files", function () {
-  return gulp.src("./src/files/**/*.*").pipe(gulp.dest("./dist/files/"));
+  return gulp.src("./src/files/**/*.*")
+  .pipe(changed("./dist/files/"))
+  .pipe(gulp.dest("./dist/files/"));
 });
+
+gulp.task("scripts", function () {
+  return gulp
+    .src("./src/js/**/*.js")
+	.pipe(changed("./dist/js/"))
+    .pipe(plumber(plumberNotify("JS")))
+    .pipe(babel())
+    .pipe(webpack(require("./webpack.config")))
+    .pipe(gulp.dest("./dist/js"));
+});
+
 gulp.task("server", function () {
-  return gulp.src("./dist/").pipe(
+  return gulp.src("./dist/pages").pipe(
     server({
       livereload: true,
       open: true,
@@ -76,13 +100,14 @@ gulp.task("watch", function () {
   gulp.watch("./src/img/**/*", gulp.parallel("images"));
   gulp.watch("./src/fonts/**/*", gulp.parallel("fonts"));
   gulp.watch("./src/files/**/*", gulp.parallel("files"));
+  gulp.watch("./src/js/**/*.js", gulp.parallel("scripts"));
 });
 
 gulp.task(
   "default",
   gulp.series(
     "clean",
-    gulp.parallel("html", "sass", "images", "fonts", "files"),
+    gulp.parallel("html", "sass", "images", "fonts", "files", "scripts"),
     gulp.parallel("watch", "server")
   )
 );
