ba650a8c7cfca558c6b914549d77099d74371921
egorsv1985 <egorvs2009@gmail.com>
2024-01-12 10:33:15 +0300

    fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,8 @@ const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
 const changed = require('gulp-changed')
 const sassGlob = require('gulp-sass-glob')
-const groupMedia = require('gulp-css-mqpacker')
 const postcss = require('gulp-postcss')
 const imagemin = require('gulp-imagemin')
-// const autoprefixer = require('gulp-autoprefixer')
 const webp = require('gulp-webp')
 const webpHTML = require('gulp-webp-html')
 const webImagesCSS = require('gulp-web-images-css')
@@ -66,12 +64,19 @@ const serverOptions = {
 	server: {
 		baseDir: paths.dest.dev,
 	},
-	port: 3000, // Порт по умолчанию
+	port: 3000,
+	open: true,
+	notify: false,
+}
+const customerServerOptions = {
+	server: {
+		baseDir: paths.dest.dev,
+	},
+	port: 4000,
 	open: true,
 	notify: false,
 }
 
-// Function to handle plumber errors
 const plumberNotify = title => {
 	return {
 		errorHandler: notify.onError({
@@ -82,7 +87,6 @@ const plumberNotify = title => {
 	}
 }
 
-// Helper function for clean task
 const cleanTask = function (done) {
 	const destination = paths.dest.dev
 	if (fs.existsSync(destination)) {
@@ -91,7 +95,6 @@ const cleanTask = function (done) {
 	done()
 }
 
-// HTML Task
 gulp.task('html', function () {
 	const destination = paths.dest.dev
 	const filterHTML = filter(['**/*.html', '!**/_*.html'], { restore: true })
@@ -109,35 +112,29 @@ gulp.task('html', function () {
 		.pipe(browserSync.stream())
 })
 
-// Styles Task
 gulp.task('sass', function () {
 	const destination = paths.dest.dev + 'css/'
 	const filterScss = filter(['**/*', '!src/scss/**/_*.scss'], {
 		restore: true,
 	})
 
-	return (
-		gulp
-			.src(paths.src.scss)
-			.pipe(changed(destination))
-			.pipe(plumber(plumberNotify('SCSS')))
-			.pipe(sourceMaps.init())
-			// .pipe(gulpIf(isProduction, autoprefixer(autoprefixerOptions)))
-			.pipe(sassGlob())
-			.pipe(sass(sassOptions))
-			// .pipe(gulpIf(isProduction, groupMedia()))
-			.pipe(replace('@img', paths.img.css))
-			.pipe(postcss())
-			.pipe(gulpIf(isProduction, webImagesCSS({ mode: 'all' })))
-			.pipe(sourceMaps.write())
-			.pipe(filterScss)
-			.pipe(gulp.dest(destination))
-			.pipe(filterScss.restore)
-			.pipe(browserSync.stream())
-	)
+	return gulp
+		.src(paths.src.scss)
+		.pipe(changed(destination))
+		.pipe(plumber(plumberNotify('SCSS')))
+		.pipe(sourceMaps.init())
+		.pipe(sassGlob())
+		.pipe(sass(sassOptions))
+		.pipe(replace('@img', paths.img.css))
+		.pipe(gulpIf(isProduction, postcss()))
+		.pipe(gulpIf(isProduction, webImagesCSS({ mode: 'all' })))
+		.pipe(sourceMaps.write())
+		.pipe(filterScss)
+		.pipe(gulp.dest(destination))
+		.pipe(filterScss.restore)
+		.pipe(browserSync.stream())
 })
 
-// Images Task
 gulp.task('images', function () {
 	const destination = paths.dest.dev + 'images/'
 	return gulp
@@ -150,7 +147,7 @@ gulp.task('images', function () {
 		.pipe(gulp.dest(destination))
 		.pipe(gulpIf(isProduction, imagemin({ verbose: true })))
 		.pipe(gulp.dest(destination))
-		.pipe(browserSync.stream()) // Обновление браузера
+		.pipe(browserSync.stream())
 })
 
 // Fonts Task
@@ -160,7 +157,7 @@ gulp.task('fonts', function () {
 		.src(paths.src.fonts)
 		.pipe(changed(destination))
 		.pipe(gulp.dest(destination))
-		.pipe(browserSync.stream()) // Обновление браузера
+		.pipe(browserSync.stream())
 })
 
 // Files Task
@@ -170,7 +167,7 @@ gulp.task('files', function () {
 		.src(paths.src.files)
 		.pipe(changed(destination))
 		.pipe(gulp.dest(destination))
-		.pipe(browserSync.stream()) // Обновление браузера
+		.pipe(browserSync.stream())
 })
 
 // JavaScript Task
@@ -183,10 +180,9 @@ gulp.task('js', function () {
 		.pipe(replace('@img', paths.img.js))
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest(destination))
-		.pipe(browserSync.stream()) // Обновление браузера
+		.pipe(browserSync.stream())
 })
 
-// Archive Task
 gulp.task('archive', function () {
 	const source = paths.dest.dev + '**/*'
 	const destination = './'
@@ -197,32 +193,41 @@ gulp.task('archive', function () {
 		.pipe(gulp.dest(destination))
 })
 
-// Server Task
 gulp.task('server', function () {
 	browserSync.init(serverOptions)
 })
 
-// Watch Task
-gulp.task('watch', function () {
-	gulp.watch(paths.src.scss, gulp.parallel('sass'))
-	gulp.watch(paths.src.html, gulp.parallel('html'))
-	gulp.watch(paths.src.images, gulp.parallel('images'))
-	gulp.watch(paths.src.fonts, gulp.parallel('fonts'))
-	gulp.watch(paths.src.files, gulp.parallel('files'))
-	gulp.watch(paths.src.js, gulp.parallel('js'))
+gulp.task('server-customer', function () {
+	browserSync.init(customerServerOptions)
 })
 
-// Build Task
+gulp.task('watch', function () {
+	gulp.watch(paths.src.scss, gulp.series('sass'))
+	gulp.watch(paths.src.html, gulp.series('html'))
+	gulp.watch(paths.src.images, gulp.series('images'))
+	gulp.watch(paths.src.fonts, gulp.series('fonts'))
+	gulp.watch(paths.src.files, gulp.series('files'))
+	gulp.watch(paths.src.js, gulp.series('js'))
+})
+
 gulp.task(
-	'build',
+	'dev',
 	gulp.series(
 		cleanTask,
-		gulp.parallel('html', 'sass', 'images', 'fonts', 'files', 'js')
+		gulp.parallel('html', 'sass', 'images', 'fonts', 'files', 'js'),
+		gulp.parallel('server', 'watch')
 	)
 )
 
-// Zip Task
-gulp.task('zip', gulp.series('build', 'archive'))
+gulp.task(
+	'docs',
+	gulp.series(
+		cleanTask,
+		gulp.parallel('html', 'sass', 'images', 'fonts', 'files', 'js'),
+		'server-customer'
+	)
+)
 
-// Default Task
-gulp.task('default', gulp.series('build', gulp.parallel('server', 'watch')))
+gulp.task('zip', gulp.series('docs', 'archive'))
+
+gulp.task('default', gulp.series('dev'))
