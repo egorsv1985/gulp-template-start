7699b3c89872a55d32d10196a4752b5be5bb2e18
egorsv1985 <egorvs2009@gmail.com>
2024-01-18 10:50:28 +0300

    fix bugs with styles

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,33 +1,3 @@
-// import {
-// 	cssDestination,
-// 	plumberNotify,
-// 	sassOptions,
-// 	isProduction,
-// 	filterScss,
-// 	postcssPlugins,
-// 	useTailwind,
-// } from '../config/options.js'
-// import { paths } from '../config/paths.js'
-// import { plugins } from '../config/plugins.js'
-
-// // Задача для сборки стилей
-// export const styles = () => {
-// 	return plugins.gulp
-// 		.src(paths.src.scss)
-// 		.pipe(plugins.changed(cssDestination))
-// 		.pipe(plugins.plumber(plumberNotify('SCSS')))
-// 		.pipe(plugins.sourceMaps.init())
-// 		.pipe(plugins.sassGlob())
-// 		.pipe(plugins.sass(sassOptions))
-// 		.pipe(plugins.replace('@img', paths.img.css))
-// 		.pipe(plugins.gulpIf(isProduction, plugins.postcss(postcssPlugins)))
-// 		.pipe(useTailwind && plugins.postcss(postcssPlugins.tailwindPlugins))
-// 		.pipe(plugins.sourceMaps.write())
-// 		.pipe(filterScss)
-// 		.pipe(plugins.gulp.dest(cssDestination))
-// 		.pipe(filterScss.restore)
-// 		.pipe(plugins.browserSync.stream())
-// }
 import {
 	cssDestination,
 	plumberNotify,
@@ -40,39 +10,24 @@ import {
 import { paths } from '../config/paths.js'
 import { plugins } from '../config/plugins.js'
 
-// Задача для сборки стилей
 export const styles = () => {
-	const {
-		gulp,
-		changed,
-		plumber,
-		sourceMaps,
-		sassGlob,
-		sass,
-		replace,
-		gulpIf,
-		postcss,
-		browserSync,
-	} = plugins
-
-	return gulp
+	return plugins.gulp
 		.src(paths.src.scss)
-		.pipe(changed(cssDestination))
-		.pipe(plumber(plumberNotify('SCSS')))
-		.pipe(sourceMaps.init())
-		.pipe(sassGlob())
-		.pipe(sass(sassOptions))
-		.pipe(replace('@img', paths.img.css))
-		.pipe(gulpIf(isProduction, postcss(postcssPlugins)))
+		.pipe(plugins.changed(cssDestination))
+		.pipe(plugins.plumber(plumberNotify('SCSS')))
+		.pipe(plugins.sourceMaps.init())
+		.pipe(plugins.sassGlob())
+		.pipe(plugins.sass(sassOptions))
+		.pipe(plugins.replace('@img', paths.img.css))
+		.pipe(plugins.gulpIf(isProduction, plugins.postcss(postcssPlugins)))
 		.pipe(
 			useTailwind
-				? postcss(postcssPlugins.tailwindPlugins)
+				? plugins.postcss(postcssPlugins.tailwindPlugins)
 				: plugins.gulp.dest(cssDestination)
 		)
-
-		.pipe(sourceMaps.write())
+		.pipe(plugins.sourceMaps.write())
 		.pipe(filterScss)
-		.pipe(gulp.dest(cssDestination))
+		.pipe(plugins.gulp.dest(cssDestination))
 		.pipe(filterScss.restore)
-		.pipe(browserSync.stream())
+		.pipe(plugins.browserSync.stream())
 }
